baca5f05fef454b7884429886576ecd3cf863a8c
Igor_ <igrmartoli@gmail.com>
2022-09-15 13:31:14 -0300

    cancel person

--- api/controllers/PessoasController.js
+++ api/controllers/PessoasController.js
@@ -1,4 +1,5 @@
 const db = require('../models/index')
+const Sequelize = require('sequelize')
 
 class PessoaController {
     static async pegaPessoasAtivas(req, res) {
@@ -112,10 +113,69 @@ class PessoaController {
         }
     }
     static async deleteMatricula (req, res) {
-        const {estudanteId, matriculaId} = req.params;
+        const {matriculaId} = req.params;
         try {
             await db.Matriculas.destroy({where: {id: Number(matriculaId)}});
             return res.status(200).json({success: true});
+        }catch (err) {
+            return res.status(500).json({error: err.message});
+        }
+    }
+
+    static async pegaMatriculas(req, res) {
+        const {estudanteId} = req.params;
+        try {
+            const pessoa = await db.Pessoas.findOne({where: {id: Number(estudanteId)}})
+            const matriculas = await pessoa.getAulasMatriculadas();
+            return res.status(200).json(matriculas);
+        }catch (err) {
+            return res.status(500).json({error: err.message});
+        }
+    }
+
+    static async pegaMatriculasPorTurma(req, res) {
+        const {turmaId} = req.params;
+        try {
+            const todasAsMatriculas = db.Matriculas.findAndCountAll({
+                where: {
+                turma_id: Number(turmaId),
+                status: 'confirmado'
+                },
+                limit: 20,
+                order: [['estudante_id', 'ASC']]
+        })
+            return res.status(200).json(todasAsMatriculas)
+        }catch (err) {
+            return res.status(500).json({error: err.message});
+        }
+    }
+
+    static async pegaTurmasLotadas(req, res) {
+        const lotacaoTurma = 2;
+        try {
+            const turmasLotadas = await db.Matriculas.findAndCountAll({
+                where: {
+                    status: 'confirmado'
+                },
+                attributes: ['turma_id'],
+                group: ['turma_id'],
+                having: Sequelize.literal(`count(turma_id) >= ${lotacaoTurma}`)
+            })
+
+            res.status(200).json(turmasLotadas.count)
+        }catch (err) {
+            return res.status(500).json({error: err.message});
+        }
+    }
+
+    static async cancelaPessoa(req, res) {
+        const estudanteId = req.params;
+        try {
+            await db.Pessoas
+            .update({ativo: false}, {where: {id: Number(estudanteId)}})
+            await db.Matriculas
+            .update({status: 'cancelado'}, {where: {estudante_id: Number(estudanteId)}})
+            res.status(200).json({success: true})
         }catch (err) {
             return res.status(500).json({error: err.message});
         }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -3,9 +3,11 @@ const PessoaController = require('../controllers/PessoasController');
 
 const router = Router();
 
+router.get('/pessoas/:estudanteId/matricula', PessoaController.pegaMatriculas)
 router.get('/pessoas', PessoaController.pegaPessoasAtivas);
 router.get('/pessoas/todos', PessoaController.pegaTodasPessoas);
 router.get('/pessoas/:id', PessoaController.findByPk);
+router.get('/pessoas/matricula/:turmaId/confirmadas', PessoaController.pegaMatriculasPorTurma)
 router.post('/pessoas', PessoaController.novaPessoa);
 router.put('/pessoas/edit/:id', PessoaController.editPessoa);
 router.delete('/pessoas/:id', PessoaController.deletePessoa);
@@ -14,5 +16,7 @@ router.post('/pessoas/:estudanteId/matricula', PessoaController.novaMatricula)
 router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula);
 router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.deleteMatricula);
 router.post('/pessoas/:id/restaura',  PessoaController.restauraPessoa)
+router.get('/pessoas/matricula/lotada', PessoaController.pegaTurmasLotadas)
+router.post('pessoas/:estudanteId/cancela', PessoaController.cancelaPessoa)
 
 module.exports = router;
